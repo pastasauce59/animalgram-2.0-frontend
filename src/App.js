401a78aa9a14b2a83c010377344b954c9c97a8cf
Michael Primavera <pastasauce59@gmail.com>
2021-11-17 19:56:24 -0500

    basic routing setup, building out navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="App">
       <h3>🚧 animalgram 2.0 🐕 under construction 🚧</h3>
+      <Navigation />
+      {/* react router dom version 6+ no longer uses "Switch", replaced with "Routes" */}
+        <Routes>
+          <Route exact path='/profile' element={<Profile/>} />
+        </Routes>
     </div>
   );
 }
---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from "react"
+// import { Navbar, Nav, Container } from 'react-bootstrap';
+
+function Navigation() {
+    return(
+        <>
+            <h1>Navbar goes here</h1>
+
+        </>
+    )
+}
+
+export default Navigation
